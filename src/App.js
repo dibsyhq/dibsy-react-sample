8353813b13cd8aaafb44376d80ca0ec1118a8890
Tanish Panjwani <tanish@paywise.io>
2021-11-03 10:40:29 +0300

    added comments and cleaned code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { EmbedWrapper, CardNumber, ExpiryDate, CardCvc } from "dibsy-react";
 import "dibsy-react/dist/index.css";
-import axios from 'axios';
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 function App() {
+
+  // State to determine whether the user has filled the form with valid values.
+  // If canSubmit is false, disable the Submit button.
+
   const [canSubmit, setCanSubmit] = useState(false);
+
+  // Function that will be called when user hits submit.
+  // It must send an API request to your backend endpoint, to request for a paymentToken.
+  // Function must take event, and submitPayment function as a parameter and call the 
+  // submitPayment function with a paymentToken.
 
   function onSubmit(e, submitPayment) {
     e.preventDefault();
@@ -16,11 +25,15 @@ function App() {
         if (res?.data) {
           submitPayment(res.data);
         } else {
-          console.log("Error while initiating payment");
+          console.log("Error while submitting payment");
         }
       })
       .catch((error) => console.log(error));
   }
+
+  // Callback function that is called when the payment is completed. 
+  // The function must take two parameters, a Boolean indicating if the payment 
+  // has been sucessful or not, and the payment object.
 
   function onPaymentComplete(success, payment) {
     console.log(success, payment);
@@ -29,16 +42,24 @@ function App() {
   return (
     <div>
       <EmbedWrapper
+        // Callback function that is called, if there is an error rendering the fields.
         onErrors={(errors) => {
-          console.log(errors)
-        }
-        }        
+          console.log(errors);
+        }}
+        // publicKey found under API keys on your Dashboard.
         publicKey="pk_test_Jew3tnxL1oI7YVbX8sDGiNE3nzZYyvvYFIwm"
+        // Callback function that determines if the checkout form should be submitted.
+        // For example, if the cardholder has entered illegal values or has not completed form,
+        // false is returned. Hint: use this to disable or enable checkout button
         onCanSubmitChange={(value) => {
           setCanSubmit(value);
         }}
+        // When payment is complete, the function is called.
         onPaymentComplete={onPaymentComplete}
       >
+
+        {/* submitPayment is a function that must be called when the customer clicks on the Submit button. */}
+        {/* isCheckoutSubmitted returns a Boolean indicating if the payment has been submitted */}
         {({ submitPayment, isCheckoutSubmitted }) => (
           <div className={"card-container"}>
             <CardNumber />
